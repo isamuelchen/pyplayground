ea5f466e74962ce9f8d6b0bdca55f30e5bd1c318
isamuelchen <samuel.chen@leocorn.com>
2023-08-27 03:56:18 +0000

    explored functions and tested curses

---
+++ curses_test.py
@@ -0,0 +1,20 @@
+import curses
+
+def window(stdscr):
+
+  sh, sw = stdscr.getmaxyx()
+  stdscr.addstr(0, 0, str(sh))
+  stdscr.addstr(1, 0, str(sw))
+  
+  y= sh / 2
+  x= sw / 2
+  stdscr .addstr(int(y), int(x), "hello")
+  while True:
+    #getch will get users
+    user_key=stdscr.getch()
+    if user_key in [27,113,81]:
+      break
+ 
+curses.wrapper(window)
+
+ 
--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+def calc(a, b):
+  c = a * 3 + b * 5 
+  return c
+
 print("hello world")
 a=1
 b=2
 print("a+b=", a + b)
+print("calc result", calc(a, b))
+#defining function for addition
