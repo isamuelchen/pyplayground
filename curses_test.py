8759e31a7e2de9c8764a3b9a4c146d7ed0711931
Samuel Chen <samuelchen@Samuels-MacBook-Pro.local>
2023-08-29 22:50:24 -0400

    centered mssge in curses test

--- curses_test.py
+++ curses_test.py
@@ -2,13 +2,16 @@ import curses
 
 def window(stdscr):
 
+  msg="hello mr bit aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+  wrd_in_mssg= len(msg)
+  hlf_of_wrd_in_mssg= wrd_in_mssg / 2
   sh, sw = stdscr.getmaxyx()
   stdscr.addstr(0, 0, str(sh))
   stdscr.addstr(1, 0, str(sw))
   
   y= sh / 2
-  x= sw / 2
-  stdscr .addstr(int(y), int(x), "hello")
+  x= sw / 2 - hlf_of_wrd_in_mssg
+  stdscr .addstr(int(y), int(x), msg)
   while True:
     #getch will get users
     user_key=stdscr.getch()
